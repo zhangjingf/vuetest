a203427cbe56bb3c5d780ded7653152124a9ff32
zhangjingf <734546749@qq.com>
2017-09-21 18:03:43 +0800

    change framework

--- src/views/header.vue
+++ src/views/header.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="header">
-    <div @cilck="back"><span class="icon-right-arrow"></span></div>
+    <div @cilck="back">
+      <span class="icon-right-arrow"></span>
+    </div>
     <div class="title" node-name="title"></div>
     <div></div>
   </div>
 </template>
 
 <script>
-  export default {
-    methods: {
-      back (event) {
-        console.log(this)
-      }
+export default {
+  methods: {
+    back(event) {
+      console.log(this)
     }
   }
+}
 </script>
 <style scoped>
 .header {
@@ -24,13 +26,16 @@
   line-height: 4.4rem;
   color: #6f6456;
 }
+
 .header>div {
   flex: 1;
 }
+
 span {
   display: inline-block;
   transform: rotate(180deg);
 }
+
 .title {
   text-align: center;
 }
